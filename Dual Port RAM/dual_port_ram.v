74c53fcfce08772c697bea09e2f76eb12e5c7f66
Jheel Makhijani <127103262+JheelMak@users.noreply.github.com>
2023-10-19 10:29:20 +0530

    Add files via upload

---
+++ Dual Port RAM/dual_port_ram.v
@@ -0,0 +1,22 @@
+module dual_port_ram(data1,data2,addr1,addr2,we1,we2,clk,q1,q2);
+input [7:0]data1,data2;
+input [5:0]addr1,addr2;
+input clk;
+input we1,we2;
+output reg [7:0] q1,q2;
+
+reg [7:0] ram[63:0];
+
+always @ (posedge clk)
+begin
+     if(we1)
+	        ram[addr1]<=data1;
+	  if(!we1)
+	        q1<=ram[addr1];
+     if(we2)
+	         ram[addr2]<=data2;
+     if(!we2)
+            q2<=ram[addr2];
+end				
+	       
+endmodule
